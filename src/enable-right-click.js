4e2934e8324c580e11600a07c2093adf96e7f16c
Kestin Goforth <kgoforth1503@gmail.com>
2019-07-03 22:34:37 -0400

    Remove divs blocking videos

--- src/enable-right-click.js
+++ src/enable-right-click.js
@@ -13,14 +13,5 @@ var observer = new MutationObserver(function(e){
 	//Remove divs covering post videos
 	deleteDivsByClassName('oujXn');
 	deleteDivsByClassName('QvAa1');
-
-	//Remove class that block right-clicking
-	//TODO: This has a bug where the first image doesn't resize until you click on it.
-	// divs = document.getElementsByClassName('y-yJ5');
-	// for(var i = 0; i < divs.length; i++){
-	// 	divs[i].addEventListener('load', function(){
-	// 		this.classList.remove('y-yJ5');
-	// 	});
-	// }
 });
 observer.observe(document.body, { childList: true, subtree: true });
