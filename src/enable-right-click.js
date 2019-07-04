6bacdf013161fd333b8017e79f7c6dfad50e9c44
Kestin Goforth <kgoforth1503@gmail.com>
2019-07-03 22:26:13 -0400

    remove divs covering videos

--- src/enable-right-click.js
+++ src/enable-right-click.js
@@ -1,11 +1,26 @@
-var observer = new MutationObserver(function(e){
-	//Remove divs covering posts
-	var photo_cover_divs = document.getElementsByClassName('_9AhH0');
-	for(var i = 0; i < photo_cover_divs.length; i++){
-		var div = photo_cover_divs[i];
+function deleteDivsByClassName(class_name){
+	var divs = document.getElementsByClassName(class_name);
+	for(var i = 0; i < divs.length; i++){
+		var div = divs[i];
 		div.parentNode.removeChild(div);
 	}
+}
 
-	//TODO: Enable right-click on stories.
+var observer = new MutationObserver(function(e){
+	//Remove divs covering post images
+	deleteDivsByClassName('_9AhH0');
+
+	//Remove divs covering post videos
+	deleteDivsByClassName('oujXn');
+	deleteDivsByClassName('QvAa1');
+
+	//Remove class that block right-clicking
+	//TODO: This has a bug where the first image doesn't resize until you click on it.
+	// divs = document.getElementsByClassName('y-yJ5');
+	// for(var i = 0; i < divs.length; i++){
+	// 	divs[i].addEventListener('load', function(){
+	// 		this.classList.remove('y-yJ5');
+	// 	});
+	// }
 });
 observer.observe(document.body, { childList: true, subtree: true });
